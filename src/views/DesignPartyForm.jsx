51c45eb8434cb892362a7dfc19402231adc995f9
Keturah Howard <keturah.dev@gmail.com>
2021-01-16 18:52:30 -0800

    create temporary form

--- src/views/DesignPartyForm.jsx
+++ src/views/DesignPartyForm.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import {Form, Text} from 'grommet'
+import {Form, FormField, Button, Box, TextInput} from 'grommet'
 
 const DesignPartyForm = () => {
+  const [value, setValue] = React.useState({});
+
   return(
-    <>  
-      {/* <Form> */}
-        <Text> This is where youll decorate the Party Room</Text>
-      {/* </Form> */}
-    </>
+    <Form
+      value={value}
+      onChange={nextValue => setValue(nextValue)}
+      onReset={() => setValue({})}
+      onSubmit={({ value }) => {console.log('value is...', value)}}
+    >
+      <FormField name="message" htmlFor="text-input-id" label="Message">
+        <TextInput id="text-input-id" name="message" />
+      </FormField>
+      <FormField name="theme" htmlFor="text-input-id" label="Theme">
+        <TextInput id="text-input-id" name="theme" />
+      </FormField>
+      <FormField name="cake" htmlFor="text-input-id" label="Cake">
+        <TextInput id="text-input-id" name="cake" />
+      </FormField>
+      <Box direction="row" gap="medium">
+        <Button type="submit" primary label="Submit" />
+        <Button type="reset" label="Reset" />
+      </Box>
+    </Form>
   )
 }
 
